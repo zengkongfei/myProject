2e3042da77e74f37de104235e2e40951452f2646
zengkongfei <zengkongfei@aladingbank.com>
2017-12-13 15:58:12 +0800

    测试

--- src/main/java/com/aldb/controller/LoginController.java
+++ src/main/java/com/aldb/controller/LoginController.java
@@ -2,9 +2,12 @@ package com.aldb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+
 public class LoginController {
+	@ResponseBody
 	@RequestMapping(value="/login")
 	public String login(){
 		return "123";
